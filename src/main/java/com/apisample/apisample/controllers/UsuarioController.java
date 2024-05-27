57508a0adb4964f146ee87f19cd66b8e5340a3c3
Alain Ruales <alainmateo01@gmail.com>
2024-05-26 20:37:23 -0500

    fix: add request body

--- src/main/java/com/apisample/apisample/controllers/UsuarioController.java
+++ src/main/java/com/apisample/apisample/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class UsuarioController{
     }
 
     @PostMapping()
-    public UsuarioModel saveUsuario(UsuarioModel usuario){
+    public UsuarioModel saveUsuario(@RequestBody UsuarioModel usuario){
         return this.usuarioService.saveUsuario(usuario);
     }
 }
